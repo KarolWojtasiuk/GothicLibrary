b468d1c4d7e2578134d94a66b5177ae875d76551
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2018-09-13 22:57:20 +0200

    Added basic class

--- src/GothicProcess.cs
+++ src/GothicProcess.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using System.Linq;
+using System.Diagnostics;
 
 namespace GothicLibrary
 {
+    public enum GothicVersion
+    {
+        Gothic1,
+        Gothic2NotR
+    }
+
     public class GothicProcess
     {
+        public GothicVersion GameVersion { get; private set; }
+        private IntPtr ProcessHandle { get; set; }
+
+        public GothicProcess(GothicVersion gameVersion, String processName)
+        {
+            this.GameVersion = gameVersion;
+            this.ProcessHandle = Kernel32.OpenProcess(Kernel32.ProcessAccessFlags.All, false, Process.GetProcessesByName(processName).FirstOrDefault().Id);
+        }
     }
 }
